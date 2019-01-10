c4c179db502af8130fd5e5219941ef8cd99d4274
Stenio Ferreira <stenio123@gmail.com>
2019-01-10 16:45:56 -0600

    Updated output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,9 @@ output "security_group_id" {
   value = "${azurerm_network_security_group.prod-sg.id}"
 }
 
+output "location" {
+  value = "${data.terraform_remote_state.resource_groups.location}"
+}
 output "resource_group_name" {
   value = "${var.resource_group_name}"
 }
